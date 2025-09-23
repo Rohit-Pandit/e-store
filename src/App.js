c01900f2b41a3f7efe8dfec959b7c4151f754b73
Rohit-Pandit <rohitpandit.190425@gmail.com>
2025-09-23 09:39:51 +0530

    whole code refactoring

--- src/App.js
+++ src/App.js
@@ -1,48 +1,22 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-import { getCategories } from './Components/Fetcher';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import ProductDetails from './Components/ProductDetails';
-import Basket from './Components/Basket';
-import Checkout from './Components/Checkout';
-import Category from './Components/Category';
-import Layout from './Components/Layout';
-import Home from './Components/Home';
-
-
 function App() {
-  const [categories, setCategories] = useState({errorMessage : '', data:[]});
+ 
+  const [result,setResult] = useState("");
 
-  const hasFetched = useRef(false);
-
-  useEffect(() => {
-    if (!hasFetched.current) {
-      hasFetched.current = true; // Ensure fetch only runs once
-      const fetchData = async()=>{
-      const responseObj = await getCategories();
-      setCategories(responseObj)
-      }
-      fetchData();
-    }
-  }, []);
-
-
-  
+  useEffect(()=>{
+       fetch("http://localhost:3001/categories")
+       .then(res => res.json())
+       .then(data=>{
+        console.log(data)
+        setResult(data)
+       })
+  },[])
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={ <Layout categories={categories}/>}>
-          <Route index element={<Home/>}/>
-            <Route path='basket' element={<Basket/>}/>
-            <Route path='checkout' element={<Checkout/>}/>
-            <Route path='categories/:categoryId/products/:productId' element={<ProductDetails/>}/> 
-            <Route path='categories/:categoryId' element={<Category/>}/>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+     <div></div>
     </>
     
   );
--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import CardContextProvider from './Contexts/cardContext';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <React.StrictMode>
-      <CardContextProvider>
         <App/>
-      </CardContextProvider>
-    
  </React.StrictMode>
 
 
